3371e87891d6405710f0a6e0a28370229aa5d382
maxime <maxime.te@protonmail.com>
2024-03-12 23:06:10 +0100

    open file

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,24 @@
 const std = @import("std");
+const macho = std.macho;
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    const stdout = std.io.getStdOut().writer();
+    var args = std.process.args().inner;
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    if (args.count < 2) {
+        try stdout.print("Usage: ./opack <mach-o file>", .{});
+        std.process.exit(0);
+    }
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    _ = args.skip();
+    const filepath = args.next().?;
+    const file = try std.fs.cwd().openFile(filepath, .{});
+    defer file.close();
 
-    try bw.flush(); // don't forget to flush!
+    const reader = file.reader();
+
+    const header = try reader.readStruct(macho.mach_header_64);
+    std.debug.print("magic: {x}\n", .{header.magic});
 }
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
+test "simple test" {}
