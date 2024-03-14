6033645b3c65dcd3c87d1d9a9b01d2179c72bee6
maxime <maxime.te@protonmail.com>
2024-03-14 15:31:29 +0100

    big clean

--- src/dumper.zig
+++ src/dumper.zig
@@ -12,12 +12,6 @@ const FileError = error{OpenFileError};
 const ParserError = error{SegCmdBoundary};
 const DumperError = anyerror || FormatError || ReadError || FileError || ParserError;
 
-const LCTypeTag = enum { main_cmd, seg64_cmd };
-const LCType = union(LCTypeTag) {
-    main_cmd: macho.entry_point_command,
-    seg64_cmd: macho.segment_command_64,
-};
-
 pub const MachOFile64 = struct {
     allocator: std.mem.Allocator,
 
@@ -69,50 +63,55 @@ pub const MachOFile64 = struct {
         }
     }
 
-    pub fn list_load_commands(self: *MachOFile64) DumperError!void {
+    pub fn list_load_commands(self: MachOFile64) DumperError!void {
         try stdout.print("{d} load commands found\n\n", .{self.header.ncmds});
         for (0..self.header.ncmds) |_| {
             const lcmd = self.reader.readStruct(macho.load_command) catch return ReadError.ReadLoadCommand;
             switch (lcmd.cmd) {
-                macho.LC.SEGMENT_64, macho.LC.MAIN => try self.dump_segment_cmd(lcmd.cmd),
+                macho.LC.SEGMENT_64 => try self.dump_segment_cmd(),
+                macho.LC.MAIN => try self.dump_entrypoint_cmd(),
                 else => try self.file.seekBy(lcmd.cmdsize - @sizeOf(macho.load_command)),
             }
         }
     }
 
-    fn dump_segment_cmd(self: *MachOFile64, seg_type: macho.LC) DumperError!void {
+    fn dump_segment_cmd(self: MachOFile64) DumperError!void {
+        const seg64_cmd = try self.safeReadStruct(macho.segment_command_64);
+        try self.file.seekBy(seg64_cmd.nsects * @sizeOf(macho.section_64));
+
+        std.debug.print("SEGMENT_64   SegName: {s: >20}\tNsects: {d}\tcmdsize: {d}\n", .{
+            seg64_cmd.segname,
+            seg64_cmd.nsects,
+            seg64_cmd.cmdsize,
+        });
+    }
+
+    fn dump_entrypoint_cmd(self: MachOFile64) DumperError!void {
+        const main_cmd = try self.safeReadStruct(macho.entry_point_command);
+        std.debug.print("MAIN         Entry: {d: >11}\n", .{main_cmd.entryoff});
+    }
+
+    fn safeReadStruct(self: MachOFile64, comptime T: type) !T {
         try self.file.seekBy(-@sizeOf(macho.load_command));
 
-        const cursor = try self.file.getPos();
-        var segment_cmd: LCType = undefined;
-        switch (seg_type) {
-            macho.LC.SEGMENT_64 => {
-                //
-                //      Create a "generic" (comptime T) utility function for that
-                //
-                const seg64_cmd = self.reader.readStruct(macho.segment_command_64) catch return ReadError.ReadLoadCommand;
-                segment_cmd = LCType{ .seg64_cmd = seg64_cmd };
-                try MachOFile64.check_boundary(cursor, try self.file.getPos(), @sizeOf(macho.segment_command_64));
-                try self.file.seekBy(seg64_cmd.nsects * @sizeOf(macho.section_64));
-            },
-            macho.LC.MAIN => {
-                const main_cmd = self.reader.readStruct(macho.entry_point_command) catch return ReadError.ReadLoadCommand;
-                segment_cmd = LCType{ .main_cmd = main_cmd };
-                try MachOFile64.check_boundary(cursor, try self.file.getPos(), @sizeOf(macho.entry_point_command));
-            },
-            else => unreachable,
-        }
+        const start_cursor = try self.file.getPos();
+        const struct_readed = self.reader.readStruct(T) catch return ReadError.ReadLoadCommand;
+        const end_cursor = try self.file.getPos();
 
-        switch (segment_cmd) {
-            .main_cmd => |main| std.debug.print("MAIN         Entry: {d: >11}\n", .{main.entryoff}),
-            .seg64_cmd => |s64| std.debug.print("SEGMENT_64   SegName: {s: >20}\tNsects: {d}\tcmdsize: {d}\n", .{ s64.segname, s64.nsects, s64.cmdsize }),
-        }
+        try MachOFile64.check_boundary(start_cursor, end_cursor, @sizeOf(T));
+
+        return struct_readed;
     }
 
     fn check_boundary(cursor_start: u64, cursor_end: u64, expect_size: u64) ParserError!void {
         const diff = cursor_end - cursor_start;
         if (diff != expect_size) {
-            std.debug.print("\nBOUNDARY\t\tbefore: {d}\tafter: {d}\tdiff: {d}\tsizeOf: {d}\n", .{ cursor_start, cursor_end, diff, expect_size });
+            std.debug.print("\nBOUNDARY\t\tbefore: {d}\tafter: {d}\tdiff: {d}\tsizeOf: {d}\n", .{
+                cursor_start,
+                cursor_end,
+                diff,
+                expect_size,
+            });
             return ParserError.SegCmdBoundary;
         }
     }
